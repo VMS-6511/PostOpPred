d4b29bf3e7ee9ebf66b27f915d8ca73338fc9d06
Vinith MS <suriyaku@gmail.com>
2019-04-05 13:28:32 -0400

    Added python files

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,91 @@
+#Author: Vinith Menon Suriyakumar, Christina Yan, Mike Kennelly
+#NetID: 13vms1
+#Course: CISC 452, Winter 2019
+
+from scipy.io import arff
+from io import StringIO
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split 
+import pandas as pd
+
+#This loads the file containing the data from the UCI Machine Learning repository
+data_file = '../data/ThoraricSurgery.arff'
+data, meta = arff.loadarff(data_file)
+
+outcomes_df = pd.DataFrame.from_records(data)
+nominal_features = ['DGN', 'PRE6',  'PRE14']
+TF_features = ['PRE7', 'PRE8', 'PRE9', 'PRE10', 'PRE11', 'PRE17', 'PRE19', 'PRE19',
+              'PRE25', 'PRE30', 'PRE32', 'Risk1Yr']
+numeric_features = ['PRE4', 'PRE5', 'AGE']
+nominal_dict = {feature: [] for feature in nominal_features}
+TF_dict = {feature: [] for feature in TF_features}
+
+# This collects all of the categorical features and true/false features so that they can be preprocessed
+for feature in nominal_features:
+    nominal_dict[feature][:] = (pd.Series(outcomes_df[feature].values).unique().tolist())
+for feature in TF_features:
+    TF_dict[feature][:] = (pd.Series(outcomes_df[feature].values).unique().tolist())
+
+#This for loop performs two of the preprocessing steps in our pipeline which are: one hot encoding
+# and conversion of true/false strings to 0 and 1 respectively
+for feature in outcomes_df.columns.values:
+    new_column = []
+    # This loop converts the categorical features into one hot encodings
+    for i, row in outcomes_df.iterrows():
+        if feature in nominal_features:
+            encoding = np.zeros(len(nominal_dict[feature])).tolist()
+            index = nominal_dict[feature].index(row[feature])
+            encoding[index] = 1
+            new_column.append(str(encoding))
+        # If the feature is a true/false feature then it is converted to a binary value
+        if feature in TF_features:
+            outcomes_df.loc[i,feature] = TF_dict[feature].index(row[feature])
+    if feature in nominal_features:
+        outcomes_df.drop([feature], axis=1)
+        outcomes_df[feature] = pd.DataFrame(new_column, columns=[feature])
+        
+# These functions sepsrate the dataset into the two classes so that the imbalance within the dataset
+# can be observed and used for the following upsampling
+Risk1Yr_1_df = outcomes_df.loc[outcomes_df['Risk1Yr'] == 1]
+Risk1Yr_0_df = outcomes_df.loc[outcomes_df['Risk1Yr'] == 0]
+
+# This for loop upsamples the patients who died within 1 year of their procedures since
+# the dataset had a class imbalance and results in an approximate 1:1 ratio of the dataset
+for i in range(4):
+    outcomes_df = outcomes_df.append(Risk1Yr_1_df)
+
+# Using Scikit-learn we split the dataset into an 80-20 training and test split that uses stratification to ensure
+# that the proportion of classes in the dataset is maintained in both the training and test sets
+outcomes_train, outcomes_test = train_test_split(outcomes_df, test_size=0.2, stratify=outcomes_df['Risk1Yr'].values.tolist())
+outcomes_train_df = pd.DataFrame(outcomes_train)
+outcomes_test_df = pd.DataFrame(outcomes_test)
+print(outcomes_train_df.head(5))
+# This function is used for standardizing the features to reduce the variance and improve the neural network
+# performance
+def standardization(x, mean, std):
+    z_scores_np = (x - mean) / std
+    return z_scores_np
+
+# This function rescales the features using the standardization function for the training set
+def train_standard_loop(keys):
+    for key in keys:
+        x_np = outcomes_train_df[key].values
+        x_np_mean = x_np.mean()
+        x_np_std = x_np.std()
+        outcomes_train_df[key] = outcomes_train_df[key].apply(standardization,args=(x_np_mean,x_np_std))
+
+# This function rescales the features using the standardization function for the test set
+def test_standard_loop(keys):
+    for key in keys[1:10]:
+        x_np = outcomes_test_df[key].values
+        x_np_mean = x_np.mean()
+        x_np_std = x_np.std()
+        outcomes_test_df[key] = outcomes_test_df[key].apply(standardization,args=(x_np_mean,x_np_std))
+
+train_standard_loop(list(['PRE4', 'PRE5', 'AGE']))
+test_standard_loop(list(['PRE4', 'PRE5', 'AGE']))
+
+outcomes_train_df.to_csv(path_or_buf='outcomes_data_train.csv',index=False)
+outcomes_test_df.to_csv(path_or_buf='outcomes_data_test.csv',index=False)
+
---
+++ train/neural_network.py
@@ -0,0 +1,178 @@
+#Author: Vinith Menon Suriyakumar, Christina Yan, Mike Kennelly
+#NetID: 13vms1
+#Course: CISC 452, Winter 2019
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.metrics import confusion_matrix, roc_auc_score
+import matplotlib.pyplot as plt
+import ast
+
+## Both of these random seeds are set to improve the reproducibility of results
+from numpy.random import seed
+seed(1)
+
+from tensorflow import set_random_seed
+set_random_seed(2)
+
+outcomes_train_df = pd.read_csv('../preprocessing/outcomes_data_train.csv')
+outcomes_test_df = pd.read_csv('../preprocessing/outcomes_data_test.csv')
+
+# Different attributes could be selected for training to determine which features provided the best classification results
+X_train = outcomes_train_df[['DGN', 'PRE6',  'PRE14', 'PRE7', 'PRE8', 'PRE9', 'PRE10', 'PRE11', 'PRE17', 'PRE19',
+              'PRE25', 'PRE30', 'PRE32', 'PRE4']].values
+y_train = outcomes_train_df[['Risk1Yr']].values
+
+X_test = outcomes_test_df[['DGN', 'PRE6',  'PRE14', 'PRE7', 'PRE8', 'PRE9', 'PRE10', 'PRE11', 'PRE17', 'PRE19',
+              'PRE25', 'PRE30', 'PRE32', 'PRE4']].values
+y_test = outcomes_test_df[['Risk1Yr']].values
+
+# This function converted the data from the CSV to a numpy array which is used for training the neural network model
+def convert_to_numpy(dataframe):
+    new_dataframe = []
+    for row in dataframe.tolist():
+        for value in range(len(row)):
+            print(isinstance(row[value], str))
+            if isinstance(row[value], str):
+                row[value] = ast.literal_eval(row[value])
+        new_row = []
+        for element in row:
+            if isinstance(element,list):
+                for value in element:
+                    new_row.append(value)
+                continue
+            new_row.append(element)
+        new_dataframe.append(new_row)
+    return np.asarray(new_dataframe)
+
+X_train = convert_to_numpy(X_train)
+X_test = convert_to_numpy(X_test)
+
+# This is our first model which was a deep neural network that used rectified linear activation units
+# Dropout was used as regularization of our network and each layer had 64 nodes
+model = Sequential()
+model.add(Dense(64, input_dim=25, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(1, activation='relu'))
+
+# [Mike] This functions compiles the above model with its associated loss function and optimizer
+# We used mean squared error as the loss function with the Adagrad optimizer
+model.compile(loss='mean_squared_error',
+              optimizer='adagrad',
+              metrics=['accuracy'])
+
+# This trains the model using 500 epochs and a batch size of 50
+model.fit(X_train, y_train,
+          epochs=500,
+          batch_size=50, verbose=1)
+
+# This was used to return the model's predictions (probabilities) on the test set
+# As well as convert the probabilities to the class labels
+y_pred_1 = model.predict(X_test)
+y_pred_bool = (y_pred_1 >= 0.5)
+
+# This evaluates the trained model on the test set
+score, acc = model.evaluate(X_test, y_test, batch_size=100)
+print('Test accuracy: ', acc)
+
+cm = confusion_matrix(y_test, y_pred_bool)
+print(cm)
+print(roc_auc_score(y_test, y_pred_1))
+
+# [Christina] This is our second model which was a shallow neural network that used hyperbolic tangent activation functions
+# Dropout was used as regularization of our network and each layer had 64 nodes
+model = Sequential()
+model.add(Dense(64, input_dim=25, activation='tanh'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation='tanh'))
+model.add(Dropout(0.1))
+model.add(Dense(1, activation='tanh'))
+
+# This functions compiles the above model with its associated loss function and optimizer
+# We used mean squared error as the loss function with the Adagrad optimizer
+model.compile(loss='mean_squared_error',
+              optimizer='adagrad',
+              metrics=['accuracy'])
+
+# This trains the model using 500 epochs and a batch size of 50
+model.fit(X_train, y_train,
+          epochs=500,
+          batch_size=50, verbose=1)
+
+# This was used to return the model's predictions (probabilities) on the test set
+# As well as convert the probabilities to the class labels
+y_pred_2 = model.predict(X_test)
+y_pred_bool = (y_pred_2 >= 0.5)
+
+# This evaluates the trained model on the test sets
+score, acc = model.evaluate(X_test, y_test, batch_size=100)
+print('Test accuracy: ', acc)
+
+cm = confusion_matrix(y_test, y_pred_bool)
+print(cm)
+print(roc_auc_score(y_test, y_pred_2))
+
+# This is our third model which was a shallow neural network that used sigmoid activation functions
+# Dropout was used as regularization of our network and each layer had 64 nodes
+model = Sequential()
+model.add(Dense(64, input_dim=25, activation='hard_sigmoid'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation='hard_sigmoid'))
+model.add(Dropout(0.1))
+model.add(Dense(1, activation='hard_sigmoid'))
+
+# [Vinith] This functions compiles the above model with its associated loss function and optimizer
+# We used binary cross entropy error as the loss function with the Adagrad optimizer
+model.compile(loss='binary_crossentropy',
+              optimizer='adagrad',
+              metrics=['accuracy'])
+
+# This trains the model using 500 epochs and a batch size of 50
+model.fit(X_train, y_train,
+          epochs=500,
+          batch_size=50, verbose=1)
+
+# This was used to return the model's predictions (probabilities) on the test set
+# As well as convert the probabilities to the class labels
+y_pred_3 = model.predict(X_test)
+y_pred_bool = (y_pred_3 >= 0.5)
+
+# This evaluates the trained model on the test sets
+score, acc = model.evaluate(X_test, y_test, batch_size=100)
+print('Test accuracy: ', acc)
+cm = confusion_matrix(y_test, y_pred_bool)
+print(cm)
+print(roc_auc_score(y_test, y_pred_3))
+
+# This section of code determines the false positive rates and true positive rates
+# for each model in addition to the AUC so that the ROC Curves can be plotted
+from sklearn.metrics import roc_curve
+fpr_keras_1, tpr_keras_1, thresholds_keras_1 = roc_curve(y_test, y_pred_1.ravel())
+from sklearn.metrics import auc
+auc_keras_1 = auc(fpr_keras_1, tpr_keras_1)
+
+fpr_keras_2, tpr_keras_2, thresholds_keras_2 = roc_curve(y_test, y_pred_2.ravel())
+auc_keras_2 = auc(fpr_keras_2, tpr_keras_2)
+
+
+fpr_keras_3, tpr_keras_3, thresholds_keras_3 = roc_curve(y_test, y_pred_3.ravel())
+auc_keras_3 = auc(fpr_keras_3, tpr_keras_3)
+
+# This code plots all of the ROC curves onto one plot so that they can be compared effectively
+plt.figure(1)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr_keras_1, tpr_keras_1, label='Model 1 (area = {:.3f})'.format(auc_keras_1))
+plt.plot(fpr_keras_2, tpr_keras_2, label='Model 2 (area = {:.3f})'.format(auc_keras_2))
+plt.plot(fpr_keras_3, tpr_keras_3, label='Model 3 (area = {:.3f})'.format(auc_keras_3))
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend(loc='best')
+plt.show()
